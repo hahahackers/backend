f32b36f09a5c91cbec582216da2bd245916d8b55
Aleksei Gorshkov <aleksei.gorshkov@adevinta.com>
2024-02-24 15:31:14 +0100

    backendiche

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+func hello(w http.ResponseWriter, req *http.Request) {
+	_, err := fmt.Fprintf(w, "ping\n")
+	if err != nil {
+		return
+	}
+}
+
+func chgk(w http.ResponseWriter, req *http.Request) {
+	tourID := strings.TrimPrefix(req.URL.Path, "/chgk/")
+	_, _ = fmt.Fprintf(w, "tourID: %s\n", tourID)
+}
+
+func main() {
+
+	http.HandleFunc("/ping", hello)
+	http.HandleFunc("/chgk/", chgk)
+
+	err := http.ListenAndServe(":8090", nil)
+	if err != nil {
+		return
+	}
+}
